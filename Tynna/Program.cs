7694c91ebef2c2a6d269489ce265e1eab914a6cc
Agustín Casado <agu.e.casado@gmail.com>
2021-10-29 20:41:05 -0300

    App de consola

---
+++ Tynna/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+//importamos las librerias necesarias para crear el bot
+using System.Threading.Tasks;
+
+
+namespace Tynna
+{
+    class Program
+    {
+        public static async Task Main(string[] args)
+            => await Startup.RunAsync(args);
+    }
+}
