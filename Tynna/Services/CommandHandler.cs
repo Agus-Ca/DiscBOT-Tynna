863a1e1afc07bb84902ba3376bae556dc10c6b61
Agustín Casado <agu.e.casado@gmail.com>
2021-11-09 19:51:29 -0300

    CommandHandler + primer comando -ping!-

--- Tynna/Services/CommandHandler.cs
+++ Tynna/Services/CommandHandler.cs
@@ -24,12 +24,35 @@ namespace Tynna.Services
             _commands = commands;
 
             _discord.Ready += OnReady;
+            _discord.MessageReceived += OnMessageReceived;
         }
 
         private Task OnReady()
         {
             Console.WriteLine($"Connected as {_discord.CurrentUser.Username}#{_discord.CurrentUser.Discriminator}");
             return Task.CompletedTask;
+        }
+
+        private async Task OnMessageReceived(SocketMessage arg)
+        {
+            var msg = arg as SocketUserMessage;
+
+            if (msg.Author.IsBot) return;
+            var context = new SocketCommandContext(_discord, msg);
+
+            int pos = 0;
+            if (msg.HasStringPrefix(_config["prefix"], ref pos) || msg.HasMentionPrefix(_discord.CurrentUser, ref pos))
+            {
+                var result = await _commands.ExecuteAsync(context, pos, _provider);
+
+                if (!result.IsSuccess)
+                {
+                    var reason = result.Error;
+
+                    await context.Channel.SendMessageAsync($"The following error occured: \n{reason}");
+                    Console.WriteLine($"The following error occured: \n{reason}");
+                }
+            }
         }
     }
 }
