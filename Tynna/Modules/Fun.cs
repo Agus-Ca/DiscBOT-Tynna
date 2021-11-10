6de025d01fef3e7ce3ab2178170a432f40239ac8
Agustín Casado <agu.e.casado@gmail.com>
2021-11-10 01:50:13 -0300

    Comando -meme-

--- Tynna/Modules/Fun.cs
+++ Tynna/Modules/Fun.cs
@@ -1,13 +1,43 @@
-﻿using System;
+﻿using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Tynna.Modules
 {
-    class Fun
+    public class Fun : ModuleBase
     {
+        [Command("meme")]
+        [Alias("reddit")]
+        public async Task Meme(string subreddit = null)
+        {
+            var search = subreddit ?? "memes";
 
+            var client = new HttpClient();
+            var result = await client.GetStringAsync($"https://reddit.com/r/{search}/random.json?limit=1");
+            if (!result.StartsWith("["))
+            {
+                await Context.Channel.SendMessageAsync("Este subreddit no existe!");
+                return;
+            }
+            JArray arr = JArray.Parse(result);
+            JObject post = JObject.Parse(arr[0]["data"]["children"][0]["data"].ToString());
+
+            var builder = new EmbedBuilder()
+                .WithImageUrl(post["url"].ToString())
+                .WithColor(new Color(108, 52, 131))
+                .WithTitle(post["title"].ToString())
+                .WithUrl("https://reddit.com" + post["permalink"].ToString())
+                .WithFooter($"🗨 {post["num_comments"]} ⬆️ {post["ups"]}");
+            var embed = builder.Build();
+
+            await Context.Channel.SendMessageAsync(null, false, embed);
+        }
     }
 }
